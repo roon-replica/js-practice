a4949f04fac8c3c8192373fa3e753628c0864637
roon <think-roon@naver.com>
2023-04-16 12:10:26 +0900

    module

---
+++ README.md
@@ -0,0 +1,8 @@
+### 목적
+- react, next 하다가 모르는 js 개념 나오면 튜토리얼식으로라도 해보기
+
+### 설치
+- extensions 
+  - live-server
+  - open in browser
+
---
+++ module/exports.js
@@ -0,0 +1,11 @@
+export function sayHi(user) {
+  return `Hello, ${user}!`;
+}
+
+export let numbers = [1,2,3,4];
+
+export class User{
+  constructor(name){
+    this.name = name;
+  }
+}
