ff7e41fde7fa8c603a82bab74887cd92d28abad9
roon <think-roon@naver.com>
2023-04-30 21:15:36 +0900

    [typescript] union types

---
+++ typescript/union_types.ts
@@ -0,0 +1,8 @@
+function or_type(numberOrStringOrBoolean: number | string | boolean) {
+    console.log(typeof numberOrStringOrBoolean, numberOrStringOrBoolean);
+}
+or_type(1);
+or_type("1");
+or_type(true);
+
+/*******************************************/
